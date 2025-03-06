3290f58d6ebf23e58697a731b4de179814e4332b
TastyGold <phoebejohns.coomer@students.mq.edu.au>
2025-03-06 14:27:09 +1100

    week1 work

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,10 +7,12 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 c = vec2(0.5,0.5);     // calculate distance to midpoint
   
-   if (d < 0.5) {
-      o_colour = vec4(u_colour, 1);
+   vec2 d= abs(vec2(p.x-c.x, p.y-c.y));
+  
+   if (d.x+d.y < 0.4) {
+      o_colour = vec4(vec3(mod(p.x*10,1),mod(p.y*10,1),u_colour.z), 1);
    }
    else {
       o_colour = vec4(0,0,0,1); // BLACK
